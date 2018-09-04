62b38fbdfe5171a06aeb6813a50aa6cfcbc78916
RogerHo <rogerhokp@gmail.com>
2018-09-05 01:23:47 +0800

    Update

--- src/EmailSendService.js
+++ src/EmailSendService.js
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@nestjs/common';
 import sgMail from '@sendgrid/mail';
-
+import { readFileSync } from 'fs';
 
 @Injectable()
 export default class EmailSendService {
@@ -18,14 +18,27 @@ export default class EmailSendService {
 
     /**
      * 
-     * @param {{emailTos: Array<string>, subject: string, content: string}  
+     * @param {{emailTos: Array<string>, subject: string, content: string, templateId:string, templateData: object, attachments: []}}  
      */
-    send({ emailTos, subject, content }) {
+    send({ emailTos, subject, content, templateId, templateData, attachments }) {
+
+        const eamilAttachments = attachments && attachments.map(({ path, filename }) => {
+            let bitmap = readFileSync(path);
+            return {
+                content: new Buffer(bitmap).toString('base64'),
+                filename,
+                contentId: 'file-' + filename
+            };
+        });
+
         const msg = {
             to: emailTos,
             from: this.emailFrom,
             subject: subject,
             html: content,
+            templateId: templateId,
+            dynamic_template_data: templateData,
+            attachments: eamilAttachments
         };
         return sgMail.send(msg);
     }
